7ccc67f5a8023d65769b9a23dbc50c9a9353f7dc
Andre-Dragon <36201034+Andre-Dragon@users.noreply.github.com>
2020-06-07 01:25:05 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-const menuBtn = document.querySelector('.burger-menu');
+const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu-sidebar');
+const container = document.querySelector('.container');
 
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
 
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('openMenu');
+    hamburger.classList.toggle('open');
+});
+
+container.addEventListener('click', event => {
+        const target = event.target;
+        if (!target.closest('.menu-sidebar')) {
+            menu.classList.remove('openMenu');
+            hamburger.classList.remove('open');
+        }
 });
