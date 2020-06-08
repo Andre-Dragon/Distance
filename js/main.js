1456638efdea264f6abdca96bbf440262cc8a6eb
Andre-Dragon <36201034+Andre-Dragon@users.noreply.github.com>
2020-06-08 22:22:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu-sidebar');
-const container = document.querySelector('.container');
+'use strict';
 
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu-sidebar'),
+    container = document.querySelector('.container'),
+    postShare = document.querySelector('.post-share'),
+    shareButton = document.querySelector('.share-button');
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('openMenu');
     hamburger.classList.toggle('open');
 });
+
+shareButton.addEventListener('click', () => {
+        postShare.classList.toggle('open-share');    
+});
+
 
 container.addEventListener('click', event => {
         const target = event.target;
